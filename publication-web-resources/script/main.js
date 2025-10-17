85d5c1d3a67ec6d1f474e82edaa1046cc93cecd2
Rox-94 <rodriguez12bn27@gmail.com>
2025-10-17 15:20:30 -0500

    nuevo index
    actualizazcion de archivos para nuevo index

--- publication-web-resources/script/main.js
+++ publication-web-resources/script/main.js
@@ -1,5 +1,5 @@
 var currentPage = 0;
-const totalHtmlFiles = 13;
+const totalHtmlFiles = 14;
 function changePublication() {
 	if (currentPage >= 0 && currentPage < totalHtmlFiles) {
 		var currentPageUrl = document.getElementById("contentIFrame").src;
